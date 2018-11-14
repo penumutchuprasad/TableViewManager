0519a466db3956d06e62ec77270d5a4e48feca45
leelaprasad <leela@secninjaaz.com>
2018-11-14 15:54:53 +0530

    Initial Commit

---
+++ LKStateManager/ViewController.swift
@@ -0,0 +1,23 @@
+//
+/*
+ 
+ * LKStateManager
+ * Created by: Leela Prasad on 14/11/18
+ 
+ * Copyright © 2018 Leela Prasad. All rights reserved.
+ * All rights have been granted for free of use for any project in SecNinjaz
+ 
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
